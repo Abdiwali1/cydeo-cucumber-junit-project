68b8f17ee29df03aaba5491bd0466c76ce5d59eb
adamar <aysun114@hotmail.com>
2024-06-30 10:33:28 -0500

    Vytrack task added

---
+++ src/test/java/com/cydeo/pages/VytrackLoginPage.java
@@ -0,0 +1,31 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class VytrackLoginPage {
+
+      public VytrackLoginPage(){
+          PageFactory.initElements(Driver.getDriver(),this);
+      }
+
+      @FindBy(id = "prependedInput")
+      public WebElement usernameInput;
+
+      @FindBy(id = "prependedInput2")
+      public WebElement passwordInput;
+
+      @FindBy(id = "_submit")
+      public WebElement loginBtn;
+
+
+      public void login(){
+            usernameInput.sendKeys(ConfigurationReader.getProperty("driver_username"));
+            passwordInput.sendKeys(ConfigurationReader.getProperty("driver_password"));
+            loginBtn.click();
+      }
+
+}
--- src/test/java/com/cydeo/step_definitions/VytrackLogin_StepDef.java
+++ src/test/java/com/cydeo/step_definitions/VytrackLogin_StepDef.java
@@ -1,20 +1,29 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.VytrackLoginPage;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class VytrackLogin_StepDef {
 
+    VytrackLoginPage vytrackLoginPage = new VytrackLoginPage();
 
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
-
+        Driver.getDriver().get(ConfigurationReader.getProperty("vyTrackUrl"));
     }
 
 
     @When("user enters the driver information")
     public void user_enters_the_driver_information() {
+//        vytrackLoginPage.usernameInput.sendKeys(ConfigurationReader.getProperty("driver_username"));
+//        vytrackLoginPage.passwordInput.sendKeys(ConfigurationReader.getProperty("driver_password"));
+//        vytrackLoginPage.loginBtn.click();
+
+        vytrackLoginPage.login();
 
     }
 
