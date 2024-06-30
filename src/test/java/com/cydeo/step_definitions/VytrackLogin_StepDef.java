055facff0416f37329f5df465765b953c5fc8e84
adamar <aysun114@hotmail.com>
2024-06-30 09:57:26 -0500

    Vytrack task added

---
+++ src/test/java/com/cydeo/step_definitions/VytrackLogin_StepDef.java
@@ -0,0 +1,27 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class VytrackLogin_StepDef {
+
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+
+    }
+
+
+    @When("user enters the driver information")
+    public void user_enters_the_driver_information() {
+
+    }
+
+
+    @Then("user should be able to login")
+    public void user_should_be_able_to_login() {
+
+    }
+
+}
